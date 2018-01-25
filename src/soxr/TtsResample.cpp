ef02b95f0c9f2bff36c2a26e884550e169e1109e
zhangpeng <zhangpeng@thinkit.cn>
2018-01-25 11:53:55 +0800

    tmp

---
+++ src/soxr/TtsResample.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <memory.h>
+#include "TtsResample.h"
+
+using namespace std;
+
+TtsResample::TtsResample(double irate, double orate, SRC_SRCTYPE_e src_type)
+    : m_irate(irate), m_orate(orate), m_src_type(src_type), m_olen(0)
+{
+    memset(&m_data, 0x00, sizeof(SRC_DATA));
+}
+
+TtsResample::~TtsResample()
+{
+}
+
+void TtsResample::SetIORate(double irate, double orate)
+{
+    m_irate = irate;
+    m_orate = orate;
+}
+
+void TtsResample::GetIORate(double &irate, double &orate)
+{
+    irate = m_irate;
+    orate = m_orate;
+}
+
+void TtsResample::ResampleBuf(short *data_in,  size_t inSize,
+                              short *data_out, size_t outSize)
+{
+    if (0 == m_irate || 0 == m_orate)
+    {
+        cout << "Error: Please set irate or orate!" << endl;
+        return;
+    }
+
+    m_olen = (size_t)(inSize * m_orate / m_irate + .5);
+
+    if (m_data.data_in != NULL || m_data.data_out != NULL)
+    {
+        delete m_data.data_in;
+        m_data.data_in = NULL;
+        delete m_data.data_out;
+        m_data.data_out = NULL;
+    }
+
+    m_data.data_in       = new SRC_SAMPLE[inSize];
+    m_data.data_out      = new SRC_SAMPLE[m_olen];
+    m_data.input_frames  = static_cast<long>(inSize);
+    m_data.output_frames = static_cast<long>(m_olen);
+    m_data.src_ratio     = m_orate / m_irate;
+
+    src_short_to_float_array(data_in, m_data.data_in, inSize);
+
+    // 重采样 - zhangpeng
+    int  error  = src_simple(&m_data, m_src_type, 1);
+    if (m_data.input_frames_used != m_data.input_frames)
+    {
+        cout << "WARN: the data isn't resample complete!" << endl;
+        cout << "m_data.input_frames_used " << m_data.input_frames_used << endl;
+        cout << "m_data.input_frames " << m_data.input_frames << endl;
+    }
+
+    long outLen = (m_data.output_frames_gen > outSize) ? outSize : m_data.output_frames_gen;
+
+    src_float_to_short_array(m_data.data_out, data_out, outLen);
+
+    delete m_data.data_in;
+    m_data.data_in = NULL;
+    delete m_data.data_out;
+    m_data.data_out = NULL;
+}
+
---
+++ src/soxr/TtsResample.h
@@ -0,0 +1,26 @@
+#ifndef _TTS_RESAMPLE_H_
+#define _TTS_RESAMPLE_H_
+#include <soxr-lsr.h>
+
+class TtsResample
+{
+public:
+    TtsResample(double irate = 48000,
+                double orate = 8000,
+                SRC_SRCTYPE_e src_type = SRC_SINC_FASTEST);
+    ~TtsResample();
+
+    void SetIORate(double irate, double orate);
+    void GetIORate(double &irate, double &orate);
+    void ResampleBuf(short *data_in,  size_t inSize,
+                     short *data_out, size_t outSize);
+    
+private:
+    double          m_irate;
+    double          m_orate;
+    size_t          m_olen;
+    SRC_DATA        m_data;
+    SRC_SRCTYPE_e   m_src_type;
+};
+
+#endif//_TTS_RESAMPLE_H_
