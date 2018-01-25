d0473a977aef6a1a0640c411710a7779e21f57ad
zhangpeng <zhangpeng@thinkit.cn>
2018-01-25 12:53:10 +0800

    删除

--- src/sox/ServerMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "ServerMain.h"
-
-LOGGER G_Log("./2_sox_log.log");
-
-int main(int agrs, char * argv [])
-{
-
-    return 0;
-}
--- src/sox/ServerMain.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SERVER_MAIN_H_
-#define _SERVER_MAIN_H_
-
-#include "Tit_Resample.h"
-#include "Tit_Logger.h"
-
-#endif//_SERVER_MAIN_H_
